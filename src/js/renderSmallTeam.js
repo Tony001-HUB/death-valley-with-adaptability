018ee2656ab9f0293935657d86a343eecc086b40
Anton <Osipenko>
2021-03-20 12:46:22 +0300

    render small team done

---
+++ src/js/renderSmallTeam.js
@@ -0,0 +1,37 @@
+const getData = async (url) => {
+    const res = await fetch(url);
+
+    if(!res.ok){
+       throw new Error(`Error status:" ${res.status} from ${res.url}`)
+    }
+
+    return await res.json();
+}
+
+function bindData() {
+    getData('db.json')
+        .then(data => data.person.forEach(item => { 
+            document.querySelector(".small-team_content").insertAdjacentHTML('beforeend', `
+            <li class="test">
+            <div class="test">
+            <img src="${item.img}" alt="alt">
+            <div class="small-team_container">
+            <span class="small-team_text">
+                <p class="small-team__text-merriweather">${item.name} / <span class="small-team__text-merriweather-italic">${item.position}</span><br></p>
+                <p class="small-team__text-raleway">  Lorem Ipsum is not simply is an
+                <br> action designer random text 
+                <br> It has roots in a piece</p>
+            </span>
+            <span class="small-team_icon">
+                <button type="button" class="small-team__btn btn-social-icon btn-outline-facebook"><i class="fa fa-facebook"></i></button>
+                <button type="button" class="small-team__btn btn-social-icon btn-outline-twitter"><i class="fa fa-twitter"></i></button>
+                <button type="button" class="small-team__btn btn-social-icon btn-outline-pinterest"><i class="fa fa-pinterest"></i></button>
+                <button type="button" class="small-team__btn btn-icon"> <i class="fa fa-envelope-o"></i> </button>
+            </span>
+            </div>
+            </div>
+            </li>`
+        );
+        }));
+}
+bindData();
