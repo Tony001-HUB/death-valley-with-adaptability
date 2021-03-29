e585e976dcf5b5e9c3134ca446ac6bd5d5108925
Anton <Osipenko>
2021-03-29 17:37:34 +0300

    fina: fix 1.2

--- src/js/renderSmallTeam.js
+++ src/js/renderSmallTeam.js
@@ -19,14 +19,14 @@ function bindData() {
             <span class="small-team_text">
                 <p class="small-team__text-merriweather-bubble">${item.name} / <span class="small-team__text-merriweather-italic">${item.position}</span><br></p>
                 <p class="small-team__text-raleway">  Lorem Ipsum is not simply is an
-                <br> action designer random text 
-                <br> It has roots in a piece</p>
+                action designer random text 
+                It has roots in a piece</p>
             </span>
             <span class="small-team_icon">
-                <button type="button" class="small-team__btn btn-social-icon btn-outline-facebook"><i class="fa fa-facebook"></i></button>
-                <button type="button" class="small-team__btn btn-social-icon btn-outline-twitter"><i class="fa fa-twitter"></i></button>
-                <button type="button" class="small-team__btn btn-social-icon btn-outline-pinterest"><i class="fa fa-pinterest"></i></button>
-                <button type="button" class="small-team__btn btn-icon"> <i class="fa fa-envelope-o"></i> </button>
+                <a type="button" class="small-team__btn"><i class="fa fa-facebook"></i></a>
+                <a type="button" class="small-team__btn"><i class="fa fa-twitter"></i></a>
+                <a type="button" class="small-team__btn"><i class="fa fa-pinterest"></i></a>
+                <a type="button" class="small-team__btn"> <i class="fa fa-envelope-o"></i> </a>
             </span>
             </div>
             </div>
